caec5ed695f5cda6f63928d9da3e733dc67eaf48
lazyihui <chengbaozi6@qq.com>
2024-08-08 23:55:40 +0800

    <WIP>

---
+++ Assets/Scripts_Runtime/Game_business/Domain/UserInterfaceDomain.cs
@@ -0,0 +1,18 @@
+using System;
+using UnityEngine;
+
+public static class UserInterfaceDomain {
+
+
+    public static void UpdataHandPlantPos(GameContext ctx, PlantEntity plant) {
+
+        if (plant.plantStatus == PlantStatus.Enable) {
+            return;
+        }
+
+        plant.SetPos(ctx.moduleInput.mouseWorldPos);
+
+        Debug.Log(plant.transform.position);
+    }
+
+}
