b15173521694ec2de177bb44eb90d0fa51deca52
lazyihui <chengbaozi6@qq.com>
2024-08-28 22:58:47 +0800

    <feature>add:音效目录

---
+++ Assets/Scripts_Runtime/Module_Audio/Config.cs
@@ -0,0 +1,28 @@
+
+
+
+public class Config {
+    public const string Bgm1 = "Assets/Resources/Audio/BG/nor.mp3";
+    public const string LoseWin = "Assets/Resources/Audio/effect/LoseWin.MP3";
+
+    // UI
+    public const string ClickButton = "Assets/Resources/Audio/effect/UI/ClickButton.mp3";
+    public const string Pause = "Assets/Resources/Audio/effect/UI/Pause.MP3";
+
+    public const string Shovel = "Assets/Resources/Audio/effect/UI/Shovel.mp3";
+
+    // Plant
+    public const string Plant = "Assets/Resources/Audio/effect/Plant/Plant.mp3";
+    public const string ClickSun = "Assets/Resources/Audio/effect/Plant/ClickSun.MP3";
+
+    // Zombie
+    public const string PlantAttackZombie = "Assets/Resources/Audio/effect/Zombie/PlantAttackZombie.mp3";
+    public const string ZombieEatPlant = "Assets/Resources/Audio/effect/Zombie/ZombieEatPlant.mp3";
+    public const string ZombieShout = "Assets/Resources/Audio/effect/Zombie/ZombieShout.mp3";
+
+
+
+
+
+
+}
